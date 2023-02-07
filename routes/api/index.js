a217a6bcb5c5e8849e353c0fe6a7f5bc3a1e0479
Myles Smith <smithm4949@gmail.com>
2023-02-07 11:35:51 -0500

    adding files and layout for modular routing

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const departmentRoutes = require('./departmentRoutes');
+const employeeRoutes = require('./employeeRoutes');
+const roleRoutes = require('./roleRoutes');
+
+router.use('/department', departmentRoutes);
+router.use('/employee', employeeRoutes);
+router.use('/role', roleRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(routes);
 
 // Connect to database
 const db = mysql.createConnection(
