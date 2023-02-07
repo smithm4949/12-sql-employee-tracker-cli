27eb63cd8a579d4e58e76f36ca4bcd3526fc5e43
Myles Smith <smithm4949@gmail.com>
2023-02-07 10:46:42 -0500

    server.js file created with skeleton express

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+  
